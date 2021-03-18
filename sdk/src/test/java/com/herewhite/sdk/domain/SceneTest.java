8dbf2729ac12a378ffa20253724719de85fbaef9
fenglibin <fenglibin@agora.io>
2021-03-18 15:50:15 +0800

    add model classes serialization unit test and Displayer unit test

--- sdk/src/test/java/com/herewhite/sdk/domain/SceneTest.java
+++ sdk/src/test/java/com/herewhite/sdk/domain/SceneTest.java
@@ -1,46 +1,27 @@
 package com.herewhite.sdk.domain;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import static com.herewhite.sdk.CommonTestTools.compareJson;
+import static org.junit.Assert.assertTrue;
 
 public class SceneTest {
-    private Gson gson = new Gson();
-    private static final Class[] PRIMITIVE_TYPES = { int.class, long.class, short.class,
-            float.class, double.class, byte.class, boolean.class, char.class, Integer.class, Long.class,
-            Short.class, Float.class, Double.class, Byte.class, Boolean.class, Character.class };
-    private static List<Class> PRIMITIVE_LIST = new ArrayList<>(Arrays.asList(PRIMITIVE_TYPES));
-
-    private static boolean isPrimitiveOrStringOrNull(Object target) {
-        if (target instanceof String) {
-            return true;
-        } else if (target == null) {
-            return true;
-        }
-
-        return PRIMITIVE_LIST.contains(target.getClass());
-    }
-
-    private Object toJSON(Object object) {
-
-        if (isPrimitiveOrStringOrNull(object)) {
-            return object;
-        } else if (object instanceof WhiteObject) {
-            return ((WhiteObject) object).toJSON();
-        } else {
-            return gson.toJsonTree(object);
-        }
-    }
-
     @Test
-    public void testJSON() {
+    public void serialization() {
+        Gson gson = new GsonBuilder()
+                .serializeNulls()
+                .create();
+
+
+        String old = "[{\"componentsCount\":null,\"ppt\":{\"height\":500,\"width\":500,\"src\":\"https://white-pan.oss-cn-shanghai.aliyuncs.com/101/image/alin-rusu-1239275-unsplash_opt.jpg\"},\"name\":\"page1\"},{\"ppt\":{\"height\":600,\"width\":600,\"src\":\"https://white-pan.oss-cn-shanghai.aliyuncs.com/101/image/alin-rusu-1239275-unsplash_opt.jpg\"},\"componentsCount\":null,\"name\":\"page2\"}]";
         Scene[] s = new Scene[]{
+                new Scene("page1", new PptPage("https://white-pan.oss-cn-shanghai.aliyuncs.com/101/image/alin-rusu-1239275-unsplash_opt.jpg", 500d, 500d)),
                 new Scene("page2", new PptPage("https://white-pan.oss-cn-shanghai.aliyuncs.com/101/image/alin-rusu-1239275-unsplash_opt.jpg", 600d, 600d))
         };
-        Object a = toJSON(s);
+
+        assertTrue(compareJson(old, gson.toJson(s)));
     }
 }
